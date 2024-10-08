59fb55d96f24529049d224798f62f73cafda01fa
RonnieJ507 <RonnieJ507@gmail.com>
2024-10-08 17:44:27 -0500

    adding collisions text

--- BouncingBallSim.py
+++ BouncingBallSim.py
@@ -8,12 +8,22 @@ borderWidth = 2
 pygame.init()
 
 # Create a screen (window) with aspect ration of 9:16
-width = 600
+width = 500
 height = width * 1.778
 screen = pygame.display.set_mode((width, height))
 
 # Set title of the window
 pygame.display.set_caption("Bouncing Ball Simulator")
+
+# Create a font object
+font = pygame.font.Font(None, 50)
+
+# Create the text surface
+text_surface = font.render('Collisions = 0', True, (255,255,255))
+
+# Get the rectangle of the text
+text_rect = text_surface.get_rect(center = (width/2, height - 100))
+
 
 # Main loop flag
 running = True
@@ -28,11 +38,14 @@ while running:
     # Fill the screen with a color (RGB)
     screen.fill((0, 0, 0))  # Fill with black
 
+    # Display the text onto the bottom of the screen
+    screen.blit(text_surface, text_rect)
+
     # Draw a border in the center of the screen
-    pygame.draw.circle(screen, (255, 255, 255), (width/2, height/2 - 100), borderRadius, borderWidth)
+    pygame.draw.circle(screen, (255, 255, 255), (width/2, height/2 - 50), borderRadius, borderWidth)
 
     # Draw a red circle in the center of the screen
-    pygame.draw.circle(screen, (255, 0, 0), (width/2, height/2 - 100), ballRadius)
+    pygame.draw.circle(screen, (255, 0, 0), (width/2, height/2 - 50), ballRadius)
 
     # Update the display
     pygame.display.update()
