f5256eed6fc457497e27c4e4e250ff911bef290e
RonnieJ507 <RonnieJ507@gmail.com>
2024-10-08 16:35:38 -0500

    added a border

--- BouncingBallSim.py
+++ BouncingBallSim.py
@@ -1,12 +1,15 @@
 import pygame
 
+ballRadius = 10
+borderRadius = 250
+borderWidth = 2
+
 # Initialize Pygame
 pygame.init()
 
 # Create a screen (window) with width=600 and height=800
 width = 600
 height = 800
-ballRaduis = 10
 screen = pygame.display.set_mode((width, height))
 
 # Set title of the window
@@ -25,8 +28,11 @@ while running:
     # Fill the screen with a color (RGB)
     screen.fill((0, 0, 0))  # Fill with black
 
+    # Draw a border in the center of the screen
+    pygame.draw.circle(screen, (255, 255, 255), (width/2, height/2), borderRadius, borderWidth)
+
     # Draw a red circle in the center of the screen
-    pygame.draw.circle(screen, (255, 0, 0), (width/2, height/2), ballRaduis)
+    pygame.draw.circle(screen, (255, 0, 0), (width/2, height/2), ballRadius)
 
     # Update the display
     pygame.display.update()
