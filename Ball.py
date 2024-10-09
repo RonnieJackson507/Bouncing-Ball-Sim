6fbf5d7775d8e1fc4854c5f1ff5eaa61c10817b8
RonnieJ507 <RonnieJ507@gmail.com>
2024-10-08 23:04:11 -0500

    Fixed independent Y velocity

--- Ball.py
+++ Ball.py
@@ -2,7 +2,8 @@
 class Ball:
     #Constructor
     def __init__(self, posX, posY, color):
-        self.posX = posX
-        self.posY = posY
-        self.radius = 10
-        self.color = color
+        self.posX = posX # Ball's X position
+        self.posY = posY # Ball's Y position
+        self.radius = 10 # Ball's radius
+        self.color = color # Ball's color
+        self.velY = 0 # Initial vertical velocity
--- BouncingBallSim.py
+++ BouncingBallSim.py
@@ -6,8 +6,10 @@ import Ball
 pygame.init()
 
 # Create a screen (window) with aspect ration of 9:16
-width = 500
+width = 450
 height = width * 1.778
+centerX = width / 2
+centerY = height / 2
 screen = pygame.display.set_mode((width, height))
 
 # Set title of the window
@@ -17,17 +19,19 @@ pygame.display.set_caption("Bouncing Ball Simulator")
 font = pygame.font.Font(None, 50)
 
 # Border variables
-borderX = width / 2
-borderY = height / 2 - 50
+borderX = centerX
+borderY = centerY - 50
 borderRadius = 200
 borderWidth = 2
 
 # Physics variables
-vel_y = 0 # Initial vertical velocity
 gravity = 0.5 # Gravity Constant
+bounce = 0.5 #Bounce Constant
 
 # Ball object(s)
-balls = [Ball.Ball(width/2, height/2 -50, (255,0,0))]
+balls = [Ball.Ball(centerX + 0, centerY -50, (255,0,0)),
+         Ball.Ball(centerX + 150, centerY -50, (0,255,0)),
+         Ball.Ball(centerX - 50, centerY -150, (0,0,255))]
 
 # Number of collisions from the ball
 collisions = 0
@@ -52,7 +56,7 @@ while running:
     text_surface = font.render(f"Collisions = {collisions}", True, (255,255,255))
 
     # Get the position of the text
-    text_rect = text_surface.get_rect(center = (width/2, height - 100))
+    text_rect = text_surface.get_rect(center = (centerX, height - 100))
 
     # Display the text onto the bottom of the screen
     screen.blit(text_surface, text_rect)
@@ -63,17 +67,16 @@ while running:
     # Traverse through the Balls array
     for ball in balls:
         # Apply Gravity
-        vel_y += gravity
-        ball.posY += vel_y * 0.5
+        ball.velY += gravity
+        ball.posY += ball.velY * bounce
 
         # Calculate the distance from the center of the border to the ball
         dist = math.sqrt((ball.posX - borderX) ** 2 + (ball.posY - borderY) ** 2)
 
         # Dectect Collisions
         if dist + ball.radius >= borderRadius:
-            vel_y = -vel_y
-            #Reset the ball position
-            ball.posY -= ball.radius
+            ball.velY = -ball.velY
+            ball.posY -= ball.radius #Reset the ball position
             collisions += 1
 
         # Draw the ball
